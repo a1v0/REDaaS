743169237f0c00cef76d4b10b0ce533aad1aa73e
avc285 <avc285@gmail.com>
2023-01-27 17:04:22 +0000

    create test suite with sample tests

--- REDaaSTests/UnitTest1.cs
+++ REDaaSTests/UnitTest1.cs
@@ -1,3 +1,5 @@
+using REDaaS;
+
 namespace REDaaSTests
 {
     [TestClass]
@@ -6,6 +8,8 @@ namespace REDaaSTests
         [TestMethod]
         public void TestMethod1()
         {
+            Assert.AreEqual(2, WeatherForecast.Adder(1, 1));
+            Assert.AreEqual(2, WeatherForecast.Adder(1, 3));
         }
     }
 }
